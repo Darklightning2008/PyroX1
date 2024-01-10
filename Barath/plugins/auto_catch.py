7b9f69eac1f99ac2e5111d90ab6a8382a8747c3c
epic-designer <87082611+epic-designer@users.noreply.github.com>
2024-01-10 14:35:47 +0530

    added auto_catch

--- Barath/plugins/auto_catch.py
+++ Barath/plugins/auto_catch.py
@@ -4,7 +4,7 @@ from Barath.plugins.waifu_caption import waifu_db
 from pyrogram.types import Photo
 import asyncio
 
-BOTS = [6438576771]
+BOTS = [6438576771,6883098627]
 
 @app.on_message((filters.user(BOTS) & filters.photo))
 async def guess(_, message):
@@ -19,7 +19,20 @@ async def guess(_, message):
 
             if document:
                 first_name = document.get('name', '').lower()
-                await asyncio.sleep(3)  
+                await asyncio.sleep(2)  
                 sent_message = await message.reply_text(f"/secure {first_name}")
+            else:
+                print(f"Document not found for id: {id}")
+
+        elif "/guess" in message.caption:
+      
+            id = message.photo.file_unique_id
+
+            document = await waifu_db.find_one({"id": str(id)})
+
+            if document:
+                first_name = document.get('name', '').lower()
+                await asyncio.sleep(2)  
+                sent_message = await message.reply_text(f"/guess {first_name}")
             else:
                 print(f"Document not found for id: {id}")
