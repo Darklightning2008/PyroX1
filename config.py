bd93159645b103b3e769a60cf07b70132b13e9a2
epic-designer <87082611+epic-designer@users.noreply.github.com>
2024-01-11 13:47:24 +0530

    pata ni

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 OWNER_ID = [1238234357, 5443243540]
 HANDLER = ["~", ".","!","?","@","$"]
 BARATH = "https://te.legra.ph/file/6a839cecad51014a32254.mp4"
-GROUP_ID = os.getenv("GROUP_ID", "-1001830463327")
+GROUP_ID = os.getenv("GROUP_ID", "-1002119409366")
 SOURCE = "https://t.me/SIAmKira"
-LOG_CHAT = -1001830463327
+LOG_CHAT = -1002119409366
