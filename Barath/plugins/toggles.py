e8911b70456a3ded2d4ec954b56d09f9624ee873
epic-designer <87082611+epic-designer@users.noreply.github.com>
2024-02-08 12:19:01 +0530

    done

--- Barath/plugins/toggles.py
+++ Barath/plugins/toggles.py
@@ -17,6 +17,7 @@ async def check_command_status(command_name):
     command = await toggle_db.find_one({'command_name': command_name})
     return command.get('enabled', False) if command else False
 
+# Command handler to toggle command states
 @bot.on_message(filters.command("enable") & filters.user(OWNER_ID))
 async def toggle_command_enable(client, message):
     user_id = message.from_user.id
@@ -31,14 +32,12 @@ async def toggle_command_enable(client, message):
     if command_state:
         return await message.reply_text(f"Command {command_name} is already enabled!")
     else:
-        try:
-            await toggle_db.update_one({'command_name': command_name}, {'$set': {'enabled': True}})
-            await message.reply_text(f"The command {command_name} is enabled Successfully!")
-        except Exception as err:
-            await message.reply_text(err)
+        await toggle_db.update_one({'command_name': command_name}, {'$set': {'enabled': True}})
+        return await message.reply_text(f"Command {command_name} enabled successfully!")
 
+# Command handler to toggle command states
 @bot.on_message(filters.command("disable") & filters.user(OWNER_ID))
-async def toggle_command_enable(client, message):
+async def toggle_command_disable(client, message):
     user_id = message.from_user.id
     command_name = message.text.split()[1].lower()
 
@@ -48,12 +47,8 @@ async def toggle_command_enable(client, message):
         return await message.reply_text("Command not found in the database.")
 
     command_state = command.get('enabled', False)
-    if command_state:
+    if not command_state:
         return await message.reply_text(f"Command {command_name} is already disabled!")
     else:
-        try:
-            await toggle_db.update_one({'command_name': command_name}, {'$set': {'enabled': False}})
-            await message.reply_text(f"The command {command_name} is enabled Successfully!")
-        except Exception as err:
-            await message.reply_text(err)
-    
+        await toggle_db.update_one({'command_name': command_name}, {'$set': {'enabled': False}})
+        return await message.reply_text(f"Command {command_name} disabled successfully!")
