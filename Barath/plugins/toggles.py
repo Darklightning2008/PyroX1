c5699bf1c61b8fef5a1f085686e6d441af6e85cb
epic-designer <87082611+epic-designer@users.noreply.github.com>
2024-02-08 12:08:01 +0530

    done

--- Barath/plugins/toggles.py
+++ Barath/plugins/toggles.py
@@ -1,9 +1,9 @@
 from pyrogram import filters 
-from Barath import barath
+from Barath import bot
 from Barath.barath_db.auto_catch_db import toggle_db
 from config import OWNER_ID
 
-@barath.on_message(filters.command("create_toggle") & filters.user(OWNER_ID))
+@bot.on_message(filters.command("create_toggle") & filters.user(OWNER_ID))
 async def create_toggle_command(client, message):
     if len(message.command) != 2:
         await message.reply_text("Usage: /create_toggle [command_name]")
@@ -17,7 +17,7 @@ async def check_command_status(command_name):
     command = await toggle_db.find_one({'command_name': command_name})
     return command.get('enabled', False) if command else False
 
-@barath.on_message(filters.command("enable") & filters.user(OWNER_ID))
+@bot.on_message(filters.command("enable") & filters.user(OWNER_ID))
 async def toggle_command_enable(client, message):
     user_id = message.from_user.id
     command_name = message.text.split()[1].lower()
@@ -37,7 +37,7 @@ async def toggle_command_enable(client, message):
         except Exception as err:
             message.reply_text(err)
 
-@barath.on_message(filters.command("disable") & filters.user(OWNER_ID))
+@bot.on_message(filters.command("disable") & filters.user(OWNER_ID))
 async def toggle_command_enable(client, message):
     user_id = message.from_user.id
     command_name = message.text.split()[1].lower()
