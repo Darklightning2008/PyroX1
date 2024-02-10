8e65bf70581889def807f7a8fe6d03456a77aa29
epic-designer <87082611+epic-designer@users.noreply.github.com>
2024-02-10 13:01:04 +0530

    done

--- Barath/plugins/allow_chat.py
+++ Barath/plugins/allow_chat.py
@@ -1,6 +1,6 @@
 from Barath import bot as app
 from Barath import barath
-from config import OWNER_ID,HANDLER
+from config import OWNER_ID, HANDLER
 from Barath.barath_db.auto_catch_db import allow_chats_collection
 from pyrogram import filters
 
@@ -8,24 +8,23 @@ async def get_group_name(group_id):
     group_details = await app.get_chat(group_id)
     return group_details.title
 
-async def add_to_banned_groups(group_id):
-    await allow_chats_collection.insert_one({"group_id": group_id})
+async def add_to_allowed_groups(group_id, group_name):
+    await allow_chats_collection.insert_one({"group_id": group_id, "group_name": group_name})
 
-async def remove_from_banned(group_id):
+async def remove_from_allowed(group_id):
     await allow_chats_collection.delete_one({"group_id": group_id})
 
 async def is_group_allowed(group_id):
     group = await allow_chats_collection.find_one({"group_id": group_id})
     return bool(group)
 
-async def fetch_banned_groups():
-    banned_groups = []
+async def fetch_allowed_groups():
+    allowed_groups = []
     async for group in allow_chats_collection.find():
-        banned_groups.append(f"Group ID: {group['group_id']}")
-    return banned_groups
+        allowed_groups.append(f"Group ID: {group['group_id']}, Group Name: {group['group_name']}")
+    return allowed_groups
 
-
-@barath.on_message(filters.command(["include"],prefixes=HANDLER) & filters.user(OWNER_ID))
+@barath.on_message(filters.command(["include"], prefixes=HANDLER) & filters.user(OWNER_ID))
 async def ban_group(_, message):
     idd = message.from_user.id
     if idd not in OWNER_ID:
@@ -33,20 +32,29 @@ async def ban_group(_, message):
 
     if len(message.command) > 1:
         group_id = int(message.command[1])
+        group_name = await get_group_name(group_id)
         group = await is_group_allowed(group_id)
-        if  group:
-            return await message.reply_text(f"Group is already into include  list")
+        if group:
+            return await message.reply_text(f"Group is already in the allowed list")
         try:
-            await add_to_banned_groups(group_id)
-            await message.reply_text(f"Group {group_id} is set to auto catch")
+            await add_to_allowed_groups(group_id, group_name)
+            await message.reply_text(f"Group {group_id} ({group_name}) is added to the allowed list for auto catch")
         except Exception as e:
-            await message.reply_text(f"Error banning group: {e}")
+            await message.reply_text(f"Error adding group: {e}")
     else:
-        await message.reply_text("Please provide the group ID. Example: `/include -1001830463327`")
+        group_id = message.chat.id
+        group_name = message.chat.title
+        group = await is_group_allowed(group_id)
+        if group:
+            return await message.reply_text(f"Group is already in the allowed list")
+        try:
+            await add_to_allowed_groups(group_id, group_name)
+            await message.reply_text(f"Group {group_id} ({group_name}) is added to the allowed list for auto catch")
+        except Exception as e:
+            await message.reply_text(f"Error adding group: {e}")
     await message.delete()
 
-
-@barath.on_message(filters.command("exclude",prefixes=HANDLER)& filters.user(OWNER_ID))
+@barath.on_message(filters.command("exclude", prefixes=HANDLER) & filters.user(OWNER_ID))
 async def unban_group(_, message):
     idd = message.from_user.id
     if idd not in OWNER_ID:
@@ -56,28 +64,28 @@ async def unban_group(_, message):
             group_id = int(message.command[1])
             group = await is_group_allowed(group_id)
             if group:
-                await remove_from_banned(group_id)
-                await message.reply(f"Group {group_id} is removed from allow catch.")
+                await remove_from_allowed(group_id)
+                await message.reply(f"Group {group_id} is removed from the allowed list")
             else:
-                await message.reply("Group is not into allow list")
+                await message.reply("Group is not in the allowed list")
         else:
             await message.reply("Please provide the group ID.")
     except Exception as e:
-        await message.reply_text(f"Error unbanning group: {e}")
+        await message.reply_text(f"Error removing group: {e}")
     await message.delete()
 
-@barath.on_message(filters.command("allow_chats",prefixes=HANDLER)& filters.user(OWNER_ID))
-async def get_banned_groups(_, message):
+@barath.on_message(filters.command("allow_chats", prefixes=HANDLER) & filters.user(OWNER_ID))
+async def get_allowed_groups(_, message):
     idd = message.from_user.id
     if idd not in OWNER_ID:
         return
     try:
-        banned_groups = await fetch_banned_groups()
-        if banned_groups:
-            banned_groups_list = "\n".join(banned_groups)
-            await message.reply_text(banned_groups_list)
+        allowed_groups = await fetch_allowed_groups()
+        if allowed_groups:
+            allowed_groups_list = "\n".join(allowed_groups)
+            await message.reply_text(allowed_groups_list)
         else:
-            await message.reply("No groups are currently banned.")
+            await message.reply("No groups are currently allowed.")
     except Exception as e:
-        await message.reply_text(f"Error getting banned groups: {e}")
+        await message.reply_text(f"Error getting allowed groups: {e}")
     await message.delete()
