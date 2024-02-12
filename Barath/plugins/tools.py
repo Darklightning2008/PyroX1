5f0dd09ed50631c3ed601e1c572de73a761886df
epic-designer <87082611+epic-designer@users.noreply.github.com>
2024-02-12 23:09:54 +0530

    bard done

--- Barath/plugins/tools.py
+++ Barath/plugins/tools.py
@@ -1,46 +1,70 @@
-from Barath import barath
 import requests
-from config import HANDLER,OWNER_ID
-from pyrogram import filters,types
+from pyrogram import filters, Client
+from pyrogram.types import Message, InputMediaPhoto
+from Barath import barath
+from pyrogram.errors import MediaCaptionTooLong
 
-@barath.on_message(filters.command("bard",prefixes=HANDLER) & filters.user(OWNER_ID))
-async def bard(client,message):
-    msg = await message.reply_text("Getting Response...")
-    prompt = None
-    if message.text is None:
-        prompt = None
-    if " " in message.text:
-        try:
-            return message.text.split(None, 1)[1]
-        except IndexError:
-            prompt =  None
-    else:
-        prompt = None
-    if prompt is None:
-        return await msg.edit("Where is query my master?")
-    resp = requests.post(
-        f"https://lexica.qewertyy.me/models?model_id=20&prompt={prompt}"
-        )
-    if resp.status_code != 200:
-        return await msg.edit("An error occured.")
-    data = resp.json()
+api_url_chat5 = "https://tofu-api.onrender.com/chat/bard"
+
+def fetch_data(api_url: str, query: str) -> tuple:
     try:
-        text,images = data['content'], data['images']
-        if len(images) == 0:
-            await msg.edit(text)
+        response = requests.get(f"{api_url}/{query}")
+        response.raise_for_status()
+        data = response.json()
+        return data.get("content", "No response from the API."), data.get("images", False)
+    except requests.exceptions.RequestException as e:
+        return None, f"Request error: {e}"
+    except Exception as e:
+        return None, f"An error occurred: {str(e)}"
+
+@barath.on_message(filters.command(["bard"]))
+async def chatgpt5(_, message: Message):
+    chat_id = message.chat.id
+    message_id = message.id
+    
+    if len(message.command) < 2:
+        return await message.reply_text("Please provide a query.")
+
+    query = " ".join(message.command[1:])
+    txt = await message.reply_text("Wait patiently, requesting to API...")
+    await txt.edit("💭")
+
+    api_response, images = fetch_data(api_url_chat5, query)
+
+    medias = []
+    bard = str(api_response)
+    try:
+       photo_url = images[-1]
+    except:
+        pass
+    
+    
+    if images:
+        if len(images) > 1:
+            for url in images:
+                medias.append(InputMediaPhoto(media=url, caption=None))
+                        
+            medias[-1] = InputMediaPhoto(media=photo_url, caption=bard)
+            
+            try:
+                await barath.send_media_group(chat_id=chat_id, media=medias, reply_to_message_id=message_id)
+                return await txt.delete()
+            except Exception as e:
+                return await txt.edit(str(e))
+        elif len(images) < 2:
+            image_url = images[0]
+            try:
+                await message.reply_photo(photo=image_url, caption=bard)
+                return await txt.delete()
+            except MediaCaptionTooLong:
+                return await txt.edit(bard)
+            except Exception as e:
+                return await txt.edit(str(e))
         else:
-            media = []
-            for i in images:
-                media.append(types.InputMediaPhoto(i))
-            media[0] = types.InputMediaPhoto(images[0],caption=text)
-            await barath.send_media_group(
-                message.chat.id,
-                media,
-                reply_to_message_id=message.id
-                )
+            return await txt.edit('Somthing went wrong')
+    else:
         try:
-            await message.delete()
-        except:
-            return
-    except Exception as Ok:
-        print(Ok,data)
+            return await txt.edit(bard)
+        except Exception as e:
+            return await txt.edit(str(e))
+    
