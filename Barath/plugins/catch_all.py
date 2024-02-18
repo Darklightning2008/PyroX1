daf3eacf5901a59b4ebf9d177eaa2784362c583b
epic-designer <87082611+epic-designer@users.noreply.github.com>
2024-02-18 10:26:15 +0530

    don't know

--- Barath/plugins/catch_all.py
+++ Barath/plugins/catch_all.py
@@ -1,12 +1,14 @@
 from pyrogram import Client, filters
-from Barath import barath
+
+# Create your Pyrogram client
+barath = Client("my_userbot")
 
 # Define the bot IDs and the target bot to forward messages
-source_bot_id = [6438576771]
+source_bot_id = 6438576771
 target_bot_id = 6355945378
 
 # Filter to check for messages from a particular bot in all chats
-@barath.on_message(filters.user(source_bot_id) & filters.photo & ~filters.forwarded)
+@barath.on_message(filters.from_user(source_bot_id) & filters.photo & ~filters.forwarded)
 async def forward_message(client, message):
     chat_id = message.chat.id
     # Forward the received message from source bot to the target bot
@@ -14,10 +16,10 @@ async def forward_message(client, message):
     
     # Define a filter to wait for messages from the target bot
     def from_target_bot(_, __, update):
-        return update.from_user and update.from_user.id == int(target_bot_id)
+        return update.from_user and update.from_user.id == target_bot_id
     
     # Wait for a response from the target bot
-    response = await barath.listen(from_target_bot)
+    response = await client.listen(from_target_bot)
     
     # Extract the part of the response after the "/" character including "/"
     response_parts = response.text.split('/', 1)
@@ -25,3 +27,4 @@ async def forward_message(client, message):
     
     # Once the response is received, send the extracted text back to the original chat
     await client.send_message(chat_id=chat_id, text=response_text)
+
